1dae0475a475e82ce7849dffc8b0b830cf23ad74
jane <janerajan2493@gmail.com>
2021-05-18 21:51:45 +0530

    first commit

---
+++ src/app/employee.ts
@@ -0,0 +1,13 @@
+export class Employee {
+    empId!: number;
+    firstName!: string;
+    lastName!: string;
+    emailId!: string;
+
+    // constructor(empId: number, firstName: string, lastName: string, emailId: string){
+    //     this.empId = this.empId;
+    //     this.firstName = this.firstName;
+    //     this.lastName = this.lastName;
+    //     this.emailId = this.emailId;
+    // }
+}
